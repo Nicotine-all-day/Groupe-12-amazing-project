50a912a3002b3c851c0eb7f4cf0cf755cd81df2b
Questionnes <dimos.tychalas@unine.ch>
2024-10-30 14:32:06 +0100

    More Instructions

--- hammer_bro_game.py
+++ hammer_bro_game.py
@@ -1,2 +1,5 @@
 #How to run the game
 #Run this command by the terminal : pip install ffpyplayer opencv-python
+#Then run by the terminal : python Project.py   
+#You can play either with the arrows, or the A and D buttons
+
