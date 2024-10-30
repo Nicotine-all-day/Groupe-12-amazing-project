e584dbb579d28ff05b8b45f818438dd4af5aa52a
Questionnes <dimos.tychalas@unine.ch>
2024-10-30 10:20:23 +0100

    Cleared code

--- Project.py
+++
@@ -1,4 +0,0 @@
-#This week guys(and our one girl), we need to think about what we are going to design as a game....and then start writing it here
-#Ideally as Nico Chat said, we should devide tasks between teams of people, and set specific deadlines  
-#The tic tac tow game, I added it by chat gpt, cause it would be great if everyone in the group could succesfully execute
-#a py game in their repository/Python/Vs Studio terminal, it doesn't matter
