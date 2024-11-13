a0325464219a4b5c00c0555e2a9b25274190094f
Questionnes <dimos.tychalas@unine.ch>
2024-11-13 09:04:12 +0100

    Update

--- hammer_bro_game.py
+++
@@ -1,5 +0,0 @@
-#How to run the game
-#Run this command by the terminal : pip install ffpyplayer opencv-python
-#Then run by the terminal : python Project.py   
-#You can play either with the arrows, or the A and D buttons
-
